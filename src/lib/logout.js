f9e17b1279ba0627157ec63ed40a0203af94699b
koji-webfolio-2025 <codeshift2025@gmail.com>
2025-05-06 21:43:36 +0900

    chore: remove compiled JS files and ignore in future

--- src/lib/logout.js
+++
@@ -1,4 +0,0 @@
-import api from './axios';
-export const logout = async () => {
-    await api.post('/logout');
-};
--- src/lib/tasks.js
+++
@@ -1,9 +0,0 @@
-import api from './axios';
-export const fetchTasks = async () => {
-    const res = await api.get('/tasks');
-    return res.data;
-};
-export const createTask = async (title, description) => {
-    const res = await api.post('/tasks', { title, description });
-    return res.data;
-};
